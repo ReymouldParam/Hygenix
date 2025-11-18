d6e93c40fbe5d7957c35ad09f657d305149707dd
Naresh Narnapati <naresh.narnapati@reymould.com>
2025-11-18 18:10:58 +0530

    Toggler Menu Updated in the Mobile view

--- Assets/js/home.js
+++ Assets/js/home.js
@@ -12,11 +12,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Navbar mobile
+    // NavbaR Mobile 
     const toggleBtn = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('.nav-links');
+
     toggleBtn.addEventListener('click', () => {
         navLinks.classList.toggle('active');
+        toggleBtn.classList.toggle('active');
     });
 
     // Scroll animation
